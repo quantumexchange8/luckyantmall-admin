e7c4567e88b8713021d427fa34feeb219fd27ba2
Wayne Lai <waynelai06@gmail.com>
2024-09-12 14:26:38 +0800

    feat: presets and dark mode

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,20 +1,28 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
+import dayjs from "dayjs";
+import {IconSun} from "@tabler/icons-vue";
+import { toggleDarkMode, isDark } from '@/Composables'
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-            </Link>
+    <div class="flex flex-col min-h-screen bg-surface-0 dark:bg-gray-950">
+        <div class="flex py-3 px-5 md:px-10 justify-end items-center">
+            <div
+                class="w-[60px] h-[60px] p-[17.5px] flex items-center justify-center rounded-full hover:cursor-pointer hover:bg-gray-100 text-gray-800"
+                @click="() => { toggleDarkMode() }"
+            >
+                <IconSun size="25" stroke-width="1.25" />
+            </div>
         </div>
-
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
-        >
-            <slot />
+        <div class="flex flex-1 flex-col justify-center items-center pb-12 md:px-8 xs:gap-y-[60px]">
+            <div class="w-full flex md:flex-1 justify-center">
+                <div class="w-full max-w-xs md:max-w-none md:w-[360px] flex flex-col justify-center items-center mx-5">
+                    <slot />
+                </div>
+            </div>
+            <div class="text-center text-gray-500 text-xs mt-auto">© {{ dayjs().year() }} Luckyant Mall. All rights reserved.</div>
         </div>
     </div>
 </template>
